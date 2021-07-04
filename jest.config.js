26947dd3a10785fe6e76cd0dc5bfda0779a3eceb
Lordson Vimal <47841936+lordsonvimal@users.noreply.github.com>
2021-07-04 14:25:12 +0530

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverageFrom: [
+    "**/*.{ts,tsx,js,jsx}"
+  ],
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: [
+    "/fixtures/",
+    "/packs"
+  ],
+  coverageReporters: [
+    "html",
+    "json",
+    "text",
+  ],
+  roots: [
+    "<rootDir>/src"
+  ],
+  testEnvironment: "jsdom", // Default "node"
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  // Module file extensions for importing
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+}
